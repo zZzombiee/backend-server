ec6e90c18a0d26563f36ff678f11771073dbccb7
zZzombie <itgelt.t.gi@gmail.com>
2024-09-18 12:33:00 +0800

    yeay backend started

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json);
+
+const port = 8080;
+
+app.get("/articles", (request, response) => {
+  response.send("Hello Response From Express");
+});
+
+app.post("/addArticles", (request, response) => {
+  const { title } = request.body;
+  response.send(`Product successfully added ${title}`);
+});
+
+app.listen(port, () => {
+  console.log(`server running at a http://localhost:${port}`);
+});
